7920a632699562c22b4d1cf19cfbb8bfbb1c4598
mohamedhemdan <mohamedsmsma3s@gmail.com>
2023-12-27 16:20:01 +0200

    add Repository for question entity

---
+++ Domain/Repositories/Contracts/IQuestionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories.Contracts
+{
+    public interface IQuestionRepository
+    {
+        Task<IEnumerable<Question>> GetQuestionsAsync(bool trackChanges = false);
+        Task<Question?> GetQuestionByIdAsync(Guid id,
+            bool tarckChanges = false,
+            params string[] includeProperties);
+        void CreateQuestionAsync(Question question);
+
+        void DeleteQuestionAsync(Question question);
+    }
+}
---
+++ Domain/Repositories/QuestionRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Data;
+using Domain.Entities.Models;
+using Domain.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    internal sealed class QuestionRepository : RepositoryBase<Question>, IQuestionRepository
+    {
+
+        public QuestionRepository(AppDbContext context) 
+            : base(context) { }
+        public void CreateQuestionAsync(Question question)
+            => Create(question);
+        public void DeleteQuestionAsync(Question question)
+            => Delete(question);
+        public async Task<Question?> GetQuestionByIdAsync(Guid id, bool tarckChanges = false, params string[] includeProperties)
+            => await GetByCondition(question => question.Id == id, tarckChanges, includeProperties).SingleOrDefaultAsync();
+
+        public async Task<IEnumerable<Question>> GetQuestionsAsync(bool trackChanges = false)
+            => await GetAll(trackChanges)
+            .OrderBy(question => question.Title)
+            .ToListAsync();
+    }
+}
